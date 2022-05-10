b621929419662107a2b4aa0945d41237696ec191
Bruno Da silva <brda-sil@1E4.42angouleme.fr>
2022-05-10 18:56:41 +0200

    :packge: rename main.c to so_long.c

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 16:59:52 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/05/09 17:01:57 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/05/10 18:56:08 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,42 @@
 # include "ft_printf.h"
 # include "mlx.h"
 # include <unistd.h>
+# include <stdlib.h>
+
+# define WIN_HEIGHT 640
+# define WIN_WIDTH 320
+# define WIN_TITLE "SO LONG..."
+# define BLOCK_SIZE 32
+
+// STRUCTURES
+typedef struct s_img {
+	void	*img;
+	char	*addr;
+	int		bit_per_pixel;
+	int		line_length;
+	int		endian;
+	int		height;
+	int		width;
+}				t_img;
+
+typedef struct s_textures {
+	t_img	*character;
+	t_img	*wall;
+}				t_textures;
+
+typedef struct s_main {
+	void		*mlx;
+	void		*win;
+}				t_main;
+
+// init.c
+t_img		*texture_load(t_main *config, char *path);
+void		init_texture(t_main *config, t_textures *textures);
+void		init_main(t_main *config, int height, int width, char *title);
+void		init_entry_point(t_main *config, t_textures *textures);
+
+// free.c
+void		free_texture(t_main *config, t_img *texture);
+void		free_entry_point(t_main *config, t_textures *textures);
+
 #endif
