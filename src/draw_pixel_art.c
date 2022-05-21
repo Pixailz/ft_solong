478d18a2ff9baba603ca2b355045797405e0a523
Pixailz <Pixailz1@gmail.com>
2022-05-22 00:49:41 +0200

    :construction: TODO

--- src/draw_pixel_art.c
+++ src/draw_pixel_art.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 01:54:29 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/05/21 21:41:33 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/05/21 23:58:26 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ void	print_ascii_art_pixel(t_main *config, int x, int y, int color)
 
 	i = 0;
 	j = 0;
-	while (i < BLOCK_SIZE / 4)
+	while (i < BLOCK_SIZE / 8)
 	{
-		while (j < BLOCK_SIZE / 4)
+		while (j < BLOCK_SIZE / 8)
 		{
 			mlx_pixel_put(config->mlx, config->win, \
-				(x * BLOCK_SIZE / 4) + j, \
-				(y * BLOCK_SIZE / 4) + i + \
+				(x * BLOCK_SIZE / 8) + j, \
+				(y * BLOCK_SIZE / 8) + i + \
 				(LOG_LENGTH * LOG_FONT_HEIGTH), color);
 			j++;
 		}
@@ -45,11 +45,29 @@ void	print_limits_pixel_art(t_main *config)
 						LOG_LENGTH * LOG_FONT_HEIGTH, WHITE);
 }
 
+int	print_ascii_get_g_offset(t_main *config, int length)
+{
+	int	length_in_pixel;
+	int	total_length;
+
+	length_in_pixel = length * 8;
+	total_length = config->width * 8;
+	return ((total_length - length_in_pixel) / 2);
+}
+
+//	32 / 8	= 4
+//	8 * 5	= 40
+//	13 * 8	= 104
+//	5 * 8	= 40
+//
+//
+//
+
 void	print_ascii_art_loose(t_main *config)
 {
 	int	global_offset;
 
-	global_offset = 5;
+	global_offset = print_ascii_get_g_offset(config, 5);
 	print_limits_pixel_art(config);
 	print_ascii_l(global_offset, config);
 	print_ascii_l_shadow(global_offset, config);
@@ -61,4 +79,12 @@ void	print_ascii_art_loose(t_main *config)
 	print_ascii_s_shadow(global_offset, config);
 	print_ascii_e(global_offset, config);
 	print_ascii_e_shadow(global_offset, config);
+}
+
+void	print_ascii_art_win(t_main *config)
+{
+	int	global_offset;
+
+	global_offset = print_ascii_get_g_offset(config, 3);
+
 }
